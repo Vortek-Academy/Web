6cddee3a0a36ac045d34d0af57c9607612180d9c
Ratul Saha <ratulsaha90.rs@gmail.com>
2020-11-25 13:31:55 +0530

    feat: Added VA bg on every page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
-  <v-app style="background: #141414; top: 0; left: 0">
-    <div>
-      <Navigator style="z-index: 500; position: fixed; width: 100%" />
+  <v-app style="top: 0; left: 0">
+    <div id="backdrop"></div>
+    <div id="main">
+      <Navigator style="z-index: 1000; position: fixed; width: 100%" />
       <router-view />
     </div>
   </v-app>
 </template>
 
 <script>
-import Navigator from "@/components/Navigator";
+import Navigator from '@/components/Navigator';
 
 export default {
-  name: "App",
+  name: 'App',
   components: { Navigator },
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+#backdrop {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  background: url(https://i.imgur.com/SJ3DaWC.jpg);
+  background-size: cover;
+  position: fixed;
+  overflow: hidden;
+}
+#main {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.75);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,94 +1,91 @@
 <template>
-  <v-container fluid style="top: 0; height: 100vh">
-    <div id="backdrop"></div>
-    <div id="landing">
-      <v-container
-        fluid
-        class="text-center"
-        style="color: whitesmoke; margin-top: 10rem"
+  <v-container fluid style="top: 0">
+    <v-container
+      fluid
+      class="text-center"
+      style="color: whitesmoke; margin-top: 10rem"
+    >
+      <img
+        src="https://imgur.com/tNvpxYp.png"
+        alt=""
+        style="width: 150px; height: 150px"
+      />
+      <h1
+        style="
+          font-weight: 300;
+          font-size: 3rem;
+          letter-spacing: 2px;
+          font-family: Bebas Neue, sans-serif;
+        "
       >
-        <img
-          src="https://imgur.com/tNvpxYp.png"
-          alt=""
-          style="width: 150px; height: 150px"
-        />
-        <h1
-          style="
-            font-weight: 300;
-            font-size: 3rem;
-            letter-spacing: 2px;
-            font-family: Bebas Neue, sans-serif;
-          "
-        >
-          Welcome to <span style="color: #e67e22">Vortek Academy</span>
-        </h1>
-        <br />
-        <h1
-          style="
-            font-weight: 200;
-            font-size: 2.5rem;
-            letter-spacing: 2px;
-            font-family: Bebas Neue, sans-serif;
-          "
-        >
-          <span style="color: #e67e22">The second</span> home of gamers!
-        </h1>
-      </v-container>
-      <v-container fluid class="text-center" style="margin-top: 2.4rem">
-        <h1
-          style="
-            font-weight: 300;
-            font-size: 2.1rem;
-            letter-spacing: 2px;
-            font-family: Bebas Neue, sans-serif;
-            color: whitesmoke;
-            margin-bottom: 2rem;
-          "
-        >
-          <u>Find Us On</u>
-        </h1>
-        <v-btn
-          fab
-          small
-          text
-          href="https://twitter.com/VortekAcademy"
-          target="_blank"
-          ><v-icon>mdi-twitter</v-icon></v-btn
-        >
-        <v-btn
-          fab
-          small
-          text
-          href="https://instagram.com/vortek.academy"
-          target="_blank"
-          ><v-icon>mdi-instagram</v-icon></v-btn
-        >
-        <v-btn
-          height="2.9rem"
-          depressed
-          href="https://discord.vortekacademy.com"
-          target="_blank"
-          style="border-radius: 10px"
-          ><v-icon style="margin-right: 4px">mdi-discord</v-icon>Discord</v-btn
-        >
-        <v-btn
-          fab
-          small
-          text
-          href="https://steamcommunity.com/id/vortekacademy"
-          target="_blank"
-          ><v-icon>mdi-steam</v-icon></v-btn
-        >
-        <v-btn
-          fab
-          small
-          text
-          href="https://twitch.tv/vortekacademy"
-          target="_blank"
-          ><v-icon>mdi-twitch</v-icon></v-btn
-        >
-      </v-container>
-    </div>
+        Welcome to <span style="color: #e67e22">Vortek Academy</span>
+      </h1>
+      <br />
+      <h1
+        style="
+          font-weight: 200;
+          font-size: 2.5rem;
+          letter-spacing: 2px;
+          font-family: Bebas Neue, sans-serif;
+        "
+      >
+        <span style="color: #e67e22">The second</span> home of gamers!
+      </h1>
+    </v-container>
+    <v-container fluid class="text-center" style="margin-top: 2.4rem">
+      <h1
+        style="
+          font-weight: 300;
+          font-size: 2.1rem;
+          letter-spacing: 2px;
+          font-family: Bebas Neue, sans-serif;
+          color: whitesmoke;
+          margin-bottom: 2rem;
+        "
+      >
+        <u>Find Us On</u>
+      </h1>
+      <v-btn
+        fab
+        small
+        text
+        href="https://twitter.com/VortekAcademy"
+        target="_blank"
+        ><v-icon>mdi-twitter</v-icon></v-btn
+      >
+      <v-btn
+        fab
+        small
+        text
+        href="https://instagram.com/vortek.academy"
+        target="_blank"
+        ><v-icon>mdi-instagram</v-icon></v-btn
+      >
+      <v-btn
+        height="2.9rem"
+        depressed
+        href="https://discord.vortekacademy.com"
+        target="_blank"
+        style="border-radius: 10px"
+        ><v-icon style="margin-right: 4px">mdi-discord</v-icon>Discord</v-btn
+      >
+      <v-btn
+        fab
+        small
+        text
+        href="https://steamcommunity.com/id/vortekacademy"
+        target="_blank"
+        ><v-icon>mdi-steam</v-icon></v-btn
+      >
+      <v-btn
+        fab
+        small
+        text
+        href="https://twitch.tv/vortekacademy"
+        target="_blank"
+        ><v-icon>mdi-twitch</v-icon></v-btn
+      >
+    </v-container>
   </v-container>
 </template>
 
@@ -99,25 +96,6 @@ export default {
 </script>
 
 <style scoped>
-#backdrop {
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 0;
-  background: url(https://i.imgur.com/SJ3DaWC.jpg);
-  background-size: cover;
-  position: relative;
-  overflow: hidden;
-}
-#landing {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0, 0, 0, 0.75);
-}
 .v-btn {
   background: #383a3c !important;
   margin: 0 1.1rem;
